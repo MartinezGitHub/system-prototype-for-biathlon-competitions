6568efe966e2d2648da8b602611b4b46b1765b3f
Fedorov_Maksim <409754@edu.itmo.ru>
2025-05-06 23:15:52 +0300

    delete state_machine interface

--- internal/state_machine/interface.go
+++
@@ -1,8 +0,0 @@
-package state_machine
-
-import "github.com/MartinezGitHub/system-prototype-for-biathlon-competitions/internal/entity"
-
-type StateMachine interface {
-	CanTransition(currentStatus entity.Status, event entity.EventType) bool
-	ApplyTransition(currentStatus entity.Status, event entity.EventType) (entity.Status, error)
-}
